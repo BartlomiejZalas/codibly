79881bba8a24ad5dd8e5bf6e78e7c123175ae2de
Bartlomiej Zalas <bartlomiej.zalas@nokia.com>
2020-11-24 19:48:08 +0100

    material UI theme adjusted to codibly colors

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from "./theme";
+import { ThemeProvider } from '@material-ui/core/styles';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element =
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>;
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+    palette: {
+        type: 'light',
+        primary: {
+            light: '#aff29d',
+            main: '#5adab2',
+            dark: '#0f714e',
+        },
+        secondary: {
+            light: '#aff29d',
+            main: '#5adab2',
+            dark: '#0f714e',
+        },
+    },
+});
+export default theme;
