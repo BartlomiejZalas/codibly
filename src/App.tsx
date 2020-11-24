c481f3b62c5bde5311104ba5014e2909ae848c7c
Bartlomiej Zalas <bartlomiej.zalas@nokia.com>
2020-11-24 19:36:54 +0100

    material UI introduced

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Button, TextField } from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      login screen
-    </div>
-  );
+    return (
+        <div className="App">
+            <TextField label="Login" variant="outlined"/>
+            <TextField label="Password" variant="outlined" type="password"/>
+            <Button>Login</Button>
+        </div>
+    );
 }
 
 export default App;
