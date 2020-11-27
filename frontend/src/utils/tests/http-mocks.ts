15932846bd44c17d29834daa5ecacc45f7a0556e
Bartlomiej Zalas <b.zalas@zoovu.com>
2020-11-27 22:42:56 +0100

    cors problem solved for tests

--- frontend/src/utils/tests/http-mocks.ts
+++ frontend/src/utils/tests/http-mocks.ts
@@ -2,8 +2,10 @@ import nock from 'nock';
 import { LoginApiRequestPayload, UserApiResponse } from '../../api/user/types';
 import { API_URL } from '../../constants/urls';
 
+const allowCorsHeader = { 'Access-Control-Allow-Origin': '*' };
+
 export const mockLogin = (
   bodyChecker: (body: LoginApiRequestPayload) => boolean,
   response?: UserApiResponse,
   code = 200
-) => nock(API_URL).post('/login', bodyChecker).reply(code, response);
+) => nock(API_URL).post('/login', bodyChecker).reply(code, response, allowCorsHeader);
