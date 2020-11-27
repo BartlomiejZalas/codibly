15e8a3e413357552e925e8f60122ba269571bd40
Bartlomiej Zalas <b.zalas@zoovu.com>
2020-11-27 18:55:50 +0100

    error mapping based on http response

--- frontend/src/utils/errors.ts
+++ frontend/src/utils/errors.ts
@@ -1,11 +1,25 @@
+import { AxiosError } from 'axios';
 import { ErrorCode } from '../constants/errorCodes';
 
-export const mapToErrorCode = (e: Error) => {
-  // TODO add more cases
-  return ErrorCode.GENERAL_HTTP_ERROR;
+const isAxiosError = (error: any): error is AxiosError => {
+  return (error as AxiosError).isAxiosError !== undefined;
 };
 
-export const mapCodeToMessage = (e: ErrorCode) => {
+export const mapToErrorCode = (e: AxiosError | Error): ErrorCode => {
+  if (isAxiosError(e) && e.response) {
+    switch (e.response.status) {
+      case 401:
+        return ErrorCode.CREDENTIALS_INCORRECT;
+      case 400:
+      default:
+        return ErrorCode.GENERAL_HTTP_ERROR;
+    }
+  } else {
+    return ErrorCode.GENERAL_HTTP_ERROR;
+  }
+};
+
+export const mapCodeToMessage = (e: ErrorCode): string => {
   switch (e) {
     case ErrorCode.CREDENTIALS_INCORRECT:
       return 'Incorrect login or password!';
