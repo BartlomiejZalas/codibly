ec6b30e9821d27a08a89fc8d3e1fd76ef196a231
Bartlomiej Zalas <b.zalas@zoovu.com>
2020-11-27 19:41:25 +0100

    secret page introduced

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useUserAuthentication } from './hooks/useUserAuthentication';
 import { LoginPage } from './pages/login';
+import { SecretPage } from './pages/secretPage';
 
 function App() {
-  return <LoginPage />;
+  const { isLogged } = useUserAuthentication();
+
+  if (isLogged) {
+    return <SecretPage />;
+  } else {
+    return <LoginPage />;
+  }
 }
 
 export default App;
---
+++ frontend/src/hooks/useUserAuthentication.ts
@@ -0,0 +1,12 @@
+import { useTypedSelector } from '../redux/store';
+
+export const useUserAuthentication = () => {
+  const email = useTypedSelector((state) => state.user.email);
+
+  const isLogged = !!email;
+
+  return {
+    isLogged,
+    email,
+  };
+};
